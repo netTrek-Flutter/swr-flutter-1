fbcf5b158155bf729e2288456bfb3521746c2ddb
Saban Ünlü [netTrek] <us@netTrek.de>
2021-11-19 09:18:02 +0100

    Merge Master And Navigator

--- lib/widgets/main/app.dart
+++ lib/widgets/main/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:myfirstapp/widgets/private/bloc_sample/post_bloc.dart';
 import 'package:myfirstapp/widgets/samples/button_sample.dart';
 import 'package:myfirstapp/widgets/samples/theme_sample.dart';
 
@@ -7,8 +9,10 @@ import 'home.dart';
 class MyApp extends StatelessWidget {
   final Widget? child;
   final Navigator? navigator;
+  final bool? enableBloc;
 
-  const MyApp({Key? key, this.child, this.navigator}) : super(key: key);
+  const MyApp({Key? key, this.child, this.enableBloc, this.navigator})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +20,13 @@ class MyApp extends StatelessWidget {
       theme: (child is ThemeSample || child is ButtonSample) ? theme : null,
       darkTheme: child is ThemeSample ? darkTheme : null,
       themeMode: ThemeMode.system,
-      home: (navigator != null) ? navigator : MyHome(child: child),
+      home: (navigator != null)
+          ? navigator
+          : MyHome(
+              child: (enableBloc == null || enableBloc == false)
+                  ? child
+                  : BlocProvider(create: (_) => PostBloc(), child: child),
+            ),
     );
   }
 }
